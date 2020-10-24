81bbc251b86f4bf375fe39e9abecce50a6f1e41d
RodrigoAGuspian <raguspian@misena.edu.co>
2020-10-23 21:44:03 -0500

    Finish por mi parte

--- app/src/main/java/com/proyecto/asn/proyectoasn/controllers/InteraccionActivity.java
+++ app/src/main/java/com/proyecto/asn/proyectoasn/controllers/InteraccionActivity.java
@@ -2,24 +2,144 @@ package com.proyecto.asn.proyectoasn.controllers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.proyecto.asn.proyectoasn.R;
 
-public class InteraccionActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class InteraccionActivity extends AppCompatActivity  implements OnClickListener{
+    MediaPlayer sonido;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_interaccion);
-        findViewById(R.id.btnShow).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        inicializarVista();
+        inicializarVistas();
+        sonido =  MediaPlayer.create(this,R.raw.abdomen);
+    }
+
+    // Método para inicializar el modo de presentación
+    private void inicializarVista() {
+        if(MenuActivity.modoPresentacion==0){
+            setContentView(R.layout.activity_interaccion);
+        }else{
+            setContentView(R.layout.activity_interaccion_nina);
+        }
+
+    }
+
+
+    private void inicializarVistas() {
+        findViewById(R.id.btnShow).setOnClickListener(this);
+        findViewById(R.id.btnCabeza).setOnClickListener(this);
+        findViewById(R.id.btnOjo1).setOnClickListener(this);
+        findViewById(R.id.btnOjo2).setOnClickListener(this);
+        findViewById(R.id.btnMano1).setOnClickListener(this);
+        findViewById(R.id.btnMano2).setOnClickListener(this);
+        findViewById(R.id.btnPecho).setOnClickListener(this);
+        findViewById(R.id.btnAbdomen).setOnClickListener(this);
+        findViewById(R.id.btnGenital).setOnClickListener(this);
+        findViewById(R.id.btnRodilla1).setOnClickListener(this);
+        findViewById(R.id.btnRodilla2).setOnClickListener(this);
+        findViewById(R.id.btnPie1).setOnClickListener(this);
+        findViewById(R.id.btnPie2).setOnClickListener(this);
+        findViewById(R.id.btnEspalda).setOnClickListener(this);
+        findViewById(R.id.btnCabello).setOnClickListener(this);
+        findViewById(R.id.btnTrasero).setOnClickListener(this);
+
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnShow:
                 startActivity(new Intent(InteraccionActivity.this, VideoActivity.class));
+                break;
+            case R.id.btnCabeza:
+                sonarInteraccion(R.raw.cabeza);
+                break;
+
+            case R.id.btnOjo1:
+            case R.id.btnOjo2:
+                sonarInteraccion(R.raw.ojos);
+                break;
+
+            case R.id.btnMano1:
+            case R.id.btnMano2:
+                sonarInteraccion(R.raw.manos);
+                break;
+
+            case R.id.btnPecho:
+                mostrarImgNO();
+                if(MenuActivity.modoPresentacion==0){
+                    sonarInteraccion(R.raw.pecho);
+                }else{
+                    sonarInteraccion(R.raw.busto);
+                }
+                break;
+
+            case R.id.btnAbdomen:
+                sonarInteraccion(R.raw.abdomen);
+                break;
+
+            case R.id.btnGenital:
+                mostrarImgNO();
+                if(MenuActivity.modoPresentacion==0){
+                    sonarInteraccion(R.raw.pene);
+                }else{
+                    sonarInteraccion(R.raw.vagina);
+                }
+                break;
+
+            case R.id.btnRodilla1:
+            case R.id.btnRodilla2:
+                sonarInteraccion(R.raw.rodillas);
+                break;
+
+            case R.id.btnPie1:
+            case R.id.btnPie2:
+                sonarInteraccion(R.raw.pies);
+                break;
+
+            case R.id.btnCabello:
+                sonarInteraccion(R.raw.cabello);
+                break;
+
+            case R.id.btnEspalda:
+                sonarInteraccion(R.raw.espalda);
+                break;
+
+            case R.id.btnTrasero:
+                mostrarImgNO();
+                sonarInteraccion(R.raw.trasero);
+                break;
+        }
+    }
+
+    private void mostrarImgNO(){
+        findViewById(R.id.imgNO).setVisibility(View.VISIBLE);
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                findViewById(R.id.imgNO).setVisibility(View.INVISIBLE);
             }
-        });
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask,2000);
+    }
+
+    private void sonarInteraccion(int mp3Zona) {
+        sonido.stop();
+        sonido =  MediaPlayer.create(this,mp3Zona);
+        sonido.start();
     }
 }
--- app/src/main/java/com/proyecto/asn/proyectoasn/controllers/MenuActivity.java
+++ app/src/main/java/com/proyecto/asn/proyectoasn/controllers/MenuActivity.java
@@ -1,40 +1,54 @@
 package com.proyecto.asn.proyectoasn.controllers;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.proyecto.asn.proyectoasn.R;
-import com.proyecto.asn.proyectoasn.models.Constants;
-import com.proyecto.asn.proyectoasn.models.Profesor;
 
-public class MenuActivity extends AppCompatActivity {
+import static android.view.View.*;
+
+public class MenuActivity extends AppCompatActivity implements OnClickListener {
 
     public static DatabaseReference alumnoRef;
+    public static byte modoPresentacion =0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        inicializar();
         saludarUsuario();
 
     }
+
+    // Método para inicializar los clicks de las imagenes de la vista
+    private void inicializar() {
+        findViewById(R.id.btnNino).setOnClickListener(this);
+        findViewById(R.id.btnNina).setOnClickListener(this);
+    }
+
     // Método el cual muestra un Toast de Bienvenida.
     private void saludarUsuario(){
         Toast.makeText(MenuActivity.this, R.string.welcome, Toast.LENGTH_SHORT).show();
 
     }
 
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnNino:
+                modoPresentacion = 0;
+                break;
+            case R.id.btnNina:
+                modoPresentacion = 1;
+                break;
+        }
 
+    }
 }
